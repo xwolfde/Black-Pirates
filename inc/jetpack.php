d452b224b7a5f5d3501fa0ae29640834758c3c14
Wolfgang Wiese <xwolf@xwolf.de>
2015-11-18 18:08:52 +0100

    Base structure converted from _s

---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.me/
+ *
+ * @package _s
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.me/support/infinite-scroll/
+ * See: https://jetpack.me/support/responsive-videos/
+ */
+function blackpirates_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'blackpirates_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+} // end function blackpirates_jetpack_setup
+add_action( 'after_setup_theme', 'blackpirates_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function blackpirates_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+		    get_template_part( 'template-parts/content', 'search' );
+		else :
+		    get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+} // end function blackpirates_infinite_scroll_render
