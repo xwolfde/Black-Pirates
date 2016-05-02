b8349ad10167f75251dfe067bfa3122a5c509815
Wolfgang Wiese <xwolf@xwolf.de>
2016-05-02 23:23:09 +0200

    style adjustments headers and links

--- header-searchform.php
+++ header-searchform.php
@@ -8,7 +8,7 @@ global $options;
 
 <div class="search-siteheader">    
     <h2 class="screen-reader-text"><a name="searchform"><?php _e("Search", 'blackpirates'); ?></a></h2>
-    <div class="searchwrap fa fa-search">
+    <div class="searchwrap fa fa-search 2x">
         <form method="get" class="searchform" action="<?php echo blackpirates_esc_url(home_url( '/' ))?>">
                 <label class="screen-reader-text" for="searchheader"><?php _e("Search for", 'blackpirates'); ?>:</label>
                 <input type="text" value="<?php the_search_query(); ?>" name="s" id="searchheader" 
