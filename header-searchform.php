2168ff60743172f22befa205e119310344742f3c
Wolfgang Wiese <xwolf@xwolf.de>
2016-06-19 19:01:41 +0200

    searchform

--- header-searchform.php
+++ header-searchform.php
@@ -8,14 +8,14 @@ global $options;
 
 
     <h2 class="screen-reader-text"><a name="searchform"><?php _e("Search", 'blackpirates'); ?></a></h2>
-    <div class="searchwrap fa fa-search 2x">
+    <div class="searchwrap">
         <form method="get" class="searchform" action="<?php echo blackpirates_esc_url(home_url( '/' ))?>">
                 <label class="screen-reader-text" for="searchheader"><?php _e("Search for", 'blackpirates'); ?>:</label>
                 <input type="text" value="<?php the_search_query(); ?>" name="s" id="searchheader" 
                     placeholder="<?php _e("Enter search term", 'blackpirates'); ?>"  
                     onfocus="if(this.value=='<?php _e("Enter search term", 'blackpirates'); ?>')this.value='';" onblur="if(this.value=='')this.value='<?php _e("Enter search term", 'blackpirates'); ?>';" />
-                <input type="submit" class="fa fa-search searchsubmit" value="<?php _e("Search", 'blackpirates'); ?>" />
-
+                <button class="icon"><i class="fa fa-search 2x"></i></button>
+                <input type="submit" value="<?php _e("Search", 'blackpirates'); ?>" />
         </form>
     </div>
    
